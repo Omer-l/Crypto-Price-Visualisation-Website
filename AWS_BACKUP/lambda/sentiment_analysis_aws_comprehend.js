22fbe90cccbd35865d4df216c461f26eccd405a1
Omer Kacar <omerkacar1@gmail.com>
2022-01-15 21:25:38 +0000

    feat(core): get tweet messages Lambda

--- AWS_BACKUP/lambda/sentiment_analysis_aws_comprehend.js
+++ AWS_BACKUP/lambda/sentiment_analysis_aws_comprehend.js
@@ -1,4 +1,11 @@
+//aws-sdk will be used to get DynamoDB
 let AWS = require("aws-sdk");
+
+// The database and table are 'in us-east-1'
+const ddb = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+
+//Create instance of Comprehend
+let comprehend = new AWS.Comprehend();
 
 //Text with positive sentiments about CSD3205
 let positiveText = "CSD3205 is a great course. I really love CSD3205. CSD3205 is fantastic.";
@@ -12,21 +19,63 @@ let params = {
     Text: positiveText
 };
 
-//Create instance of Comprehend
-let comprehend = new AWS.Comprehend();
+
+// Function readCryptoData
+// Reads 10 CryptoDatas from the DynamoDb table CryptoData
+// Returns promise
+function readCryptoData() {
+    const params = {
+        TableName: 'sentimentData'
+    }
+    return ddb.scan(params).promise();
+};
 
 //Function that will be called
+async function getTweets() {
+    let tweets = [];
+    try {
+        //promise awaiting
+        const Item  = await readCryptoData();
+        Item.Items.forEach(function(item) {
+            let tweetMessage = item.tweet_message;
+            tweets.push(tweetMessage);
+        });
+    }
+    catch (err) {
+        console.log("ERROR: " + JSON.stringify(err));
+    }
+    return tweets;
+}
+
 exports.handler = (event) => {
-    //Call comprehend to detect sentiment of text
-    comprehend.detectSentiment(params, (err, data) => {
-        //Log result or error
-        if (err) {
-            console.log("\nError with call to Comprehend:\n" + JSON.stringify(err));
-        }
-        else {
-            console.log("\nSuccessful call to Comprehend:\n" + JSON.stringify(data));
-        }
+    const tweets = getTweets();
+
+    tweets.then((tweetMessages) => {
+
+        // tweets.forEach(function(tweet) {
+        console.log("TWEET: " + tweetMessages);
+
+        //Parameters for call to AWS Comprehend
+        // let params = {
+        //     LanguageCode: "en",//Possible values include: "en", "es", "fr", "de", "it", "pt"
+        //     Text: tweets[0]
+        // };
+
+        // comprehend.detectSentiment(params, (comprehendErr, data) => {
+        //     //Log result or error
+        //     if (comprehendErr) {
+        //         console.log("\nError with call to Comprehend:\n" + JSON.stringify(comprehendErr));
+        //     }
+        //     else {
+        //         console.log("\nSuccessful call to Comprehend:\n" + JSON.stringify(data));
+        //     }
+        // });
     });
+
+    // return {
+    //     statusCode: 200,
+    //     body: "Ok"
+    // };
 };
 
 
