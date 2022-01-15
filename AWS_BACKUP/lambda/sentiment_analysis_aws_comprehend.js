4b57ca85f02cdfa9914bacd35eb59f5ebf14b986
Omer Kacar <omerkacar1@gmail.com>
2022-01-15 22:08:55 +0000

    refactor(core): rename functions and add comments

--- AWS_BACKUP/lambda/sentiment_analysis_aws_comprehend.js
+++ AWS_BACKUP/lambda/sentiment_analysis_aws_comprehend.js
@@ -7,23 +7,10 @@ const ddb = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
 //Create instance of Comprehend
 let comprehend = new AWS.Comprehend();
 
-//Text with positive sentiments about CSD3205
-let positiveText = "CSD3205 is a great course. I really love CSD3205. CSD3205 is fantastic.";
-
-//Text with negative sentiments about CSD3205
-let negativeText = "CSD3205 is awful. I really hate CSD3205. CSD3205 is terrible.";
-
-//Parameters for call to AWS Comprehend
-let params = {
-    LanguageCode: "en",//Possible values include: "en", "es", "fr", "de", "it", "pt"
-    Text: positiveText
-};
-
-
-// Function readCryptoData
+// Function readTwitterData
 // Reads 10 CryptoDatas from the DynamoDb table CryptoData
 // Returns promise
-function readCryptoData() {
+function readTwitterData() {
     const params = {
         TableName: 'sentimentData'
     }
@@ -35,7 +22,7 @@ async function getTweets() {
     let tweets = [];
     try {
         //promise awaiting
-        const Item  = await readCryptoData();
+        const Item  = await readTwitterData();
         Item.Items.forEach(function(item) {
             let tweetMessage = item.tweet_message;
             tweets.push(tweetMessage);
@@ -48,9 +35,9 @@ async function getTweets() {
 }
 
 exports.handler = (event) => {
-    const tweets = getTweets();
+    const tweets = getTweets(); //holds the tweets
 
-    tweets.then((tweetMessages) => {
+    tweets.then((tweetMessages) => { //after promise fulfilled
 
         tweetMessages.forEach(function(tweet) {
 
