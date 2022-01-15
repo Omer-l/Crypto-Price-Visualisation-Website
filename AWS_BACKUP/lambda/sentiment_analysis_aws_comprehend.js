0354c69b3e4812eaeb32e74ac0aa83d181989b6e
Omer Kacar <omerkacar1@gmail.com>
2022-01-15 23:01:50 +0000

    feat(core): add put sentiment analysis to DynamoDB

--- AWS_BACKUP/lambda/sentiment_analysis_aws_comprehend.js
+++ AWS_BACKUP/lambda/sentiment_analysis_aws_comprehend.js
@@ -2,7 +2,7 @@
 let AWS = require("aws-sdk");
 
 // The database and table are 'in us-east-1'
-const ddb = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+const ddb = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
 
 //Create instance of Comprehend
 let comprehend = new AWS.Comprehend();
@@ -22,10 +22,10 @@ async function getTweets() {
     let tweets = [];
     try {
         //promise awaiting
-        const Item  = await readTwitterData();
+        const Item = await readTwitterData();
         Item.Items.forEach(function(item) {
-            let tweetMessage = item.tweet_message;
-            tweets.push(tweetMessage);
+            let tweet = item;
+            tweets.push(tweet);
         });
     }
     catch (err) {
@@ -37,14 +37,18 @@ async function getTweets() {
 exports.handler = (event) => {
     const tweets = getTweets(); //holds the tweets
 
-    tweets.then((tweetMessages) => { //after promise fulfilled
+    tweets.then((tweets) => { //after promise fulfilled
 
-        tweetMessages.forEach(function(tweet) {
+        tweets.forEach(function(tweet) {
+            let tweetMessage = tweet.tweet_message;
+            let tweetDate = tweet.date;
+            console.log("DATE: " + tweetDate);
+            tweetDate = tweetDate.replace("T", " ");
 
             // Parameters for call to AWS Comprehend
             let params = {
-                LanguageCode: "en",//Possible values include: "en", "es", "fr", "de", "it", "pt"
-                Text: tweet
+                LanguageCode: "en", //Possible values include: "en", "es", "fr", "de", "it", "pt"
+                Text: tweetMessage
             };
 
             comprehend.detectSentiment(params, (comprehendErr, data) => {
@@ -52,8 +56,33 @@ exports.handler = (event) => {
                 if (comprehendErr) {
                     console.log("\nError with call to Comprehend:\n" + JSON.stringify(comprehendErr));
                 }
-                else {
-                    console.log("\nSuccessful call to Comprehend:\n" + JSON.stringify(data));
+                else { //SAMPLE : { "Sentiment": "NEUTRAL", "SentimentScore": { "Positive": 0.05800758674740791, "Negative": 0.014315063133835793, "Neutral": 0.9269210696220398, "Mixed": 0.0007562938844785094 } }
+                    console.log("\nSuccessful call to Comprehend:\n" + data.SentimentScore.Positive);
+
+                    //sends data to database.
+
+                    const ddbParams = {
+                        TableName: 'SentimentAnalysisData',
+                        LanguageCode: "en",
+                        Item: {
+                            date: tweetDate,
+                            message: tweetMessage,
+                            positiveScore: data.SentimentScore.Positive,
+                            neutralScore: data.SentimentScore.Neutral,
+                            negativeScore: data.SentimentScore.Negative,
+                        },
+                    };
+
+                    //Store data in DynamoDB and handle errors
+                    ddb.put(ddbParams, (err, data) => {
+                        if (err) {
+                            // console.error("Unable to add item", ddbParams.Item.message);
+                            console.error("Error JSON:", JSON.stringify(err));
+                        }
+                        else {
+                            console.log("Sentiment analysis added to table:");
+                        }
+                    });
                 }
             });
         });
