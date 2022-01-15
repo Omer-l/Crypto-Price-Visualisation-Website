e1674528428bee075902ce517ec050a5499db634
Omer Kacar <omerkacar1@gmail.com>
2022-01-15 17:26:21 +0000

    feat(core): add twitter GET search query

---
+++ twitter_get_sentiment.js
@@ -0,0 +1,25 @@
+
+const {TwitterApi} = require('twitter-api-v2');
+
+const client = new TwitterApi({
+    appKey: 'WQlk2KouqzcOTpMnyYyMedPfL',
+    appSecret: 'x80LCtbfosizJz6xQrSqx3L8GsUMZcc89fAWwDoL5GPXmS3ATf',
+    accessToken: '615346502-R7bC0VOZ4pyTDJNqfkBUAOxktHUFZrLkMNr6AkhP',
+    accessSecret: 'NI9xPUy0l8VVN7rXfE2w436qewMiFtKdsdX6rDfx6vHIb',
+});
+
+client.v2.search('#bitcoin', {
+    'tweet.fields': [
+        'created_at',
+    ],
+    'expansions': [
+        'author_id',
+    ],
+    'user.fields': [
+        'description',
+    ],
+}).then((val) => {
+    console.log(val.data);
+}).catch((err) => {
+    console.log(err);
+})
